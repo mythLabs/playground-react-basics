e3c5c3a09217ea5fd0247283cf114e3c7e3e6b7e
mythLabs <amitbisht744@gmail.com>
2018-11-18 11:53:28 +0530

    Render Approval component as a children

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import reactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-
+import React from "react";
+import reactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-         <CommentDetail author="Sam" timeAgo="Today at 5:34PM" content="Great one" avatar = {faker.image.avatar()}/>
-         <CommentDetail author="Andy" timeAgo="Yesterday at 3:34PM" content="Nice" avatar = {faker.image.avatar()}/>
-         <CommentDetail author="Midas" timeAgo="Today at 4:34AM" content="Amazing " avatar = {faker.image.avatar()}/>
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure ?
         </div>
-    )
-}
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 5:34PM"
+          content="Great one"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Andy"
+          timeAgo="Yesterday at 3:34PM"
+          content="Nice"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Midas"
+          timeAgo="Today at 4:34AM"
+          content="Amazing "
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-reactDOM.render(<App/>,document.querySelector('#root'));
+reactDOM.render(<App />, document.querySelector("#root"));
