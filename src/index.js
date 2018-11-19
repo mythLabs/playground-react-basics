8a3bd9b52dd975e36bd19e957b773c966379d0df
mythLabs <amitbisht744@gmail.com>
2018-11-19 19:45:43 +0530

    Refactored with lifecycle method

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import reactDOM from 'react-dom';
+import React from "react";
+import reactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-       
-        this.state= {lat: null, errorMessage: '' };
+  constructor(props) {
+    super(props);
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
-        )
+    this.state = { lat: null, errorMessage: "" };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
     }
 
-    render () {
-        return (
-          <div>
-              Latitude: {this.state.lat}
-              <br/>
-              Error: {this.state.errorMessage}
-              </div>
-          )
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
     }
+
+    return <div>Loadin!</div>;
+  }
 }
 
-reactDOM.render(<App/>,document.querySelector('#root'));
+reactDOM.render(<App />, document.querySelector("#root"));
